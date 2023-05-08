be4e0671a5c7f566fb121bb76677fb8b55320b60
Mohammad Naif <72939614+NAIF4601@users.noreply.github.com>
2023-05-09 00:24:22 +0530

    First Commit

---
+++ src/app/modules/trycs/trycs.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-trycs',
+  templateUrl: './trycs.component.html',
+  styleUrls: ['./trycs.component.scss']
+})
+
+export class TrycsComponent {
+
+}
